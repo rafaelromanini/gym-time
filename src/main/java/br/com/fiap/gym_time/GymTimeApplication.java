47c64ed9edad6275a534ffbbd629502482ac9e1c
rafaelromanini <rafaromaninideoliveira@gmail.com>
2025-03-03 17:19:05 -0300

    Project setup

---
+++ src/main/java/br/com/fiap/gym_time/GymTimeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gym_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTimeApplication.class, args);
+	}
+
+}
