270b649cd3696ced33bf87cc29a278f8b6de8c71
edvxndxvi <edvandavi1@hotmail.com>
2025-03-24 15:55:35 -0300

    Add: Endpoint update and refactoring controller

--- src/main/java/br/com/fiap/gym_time/controller/PostController.java
+++ src/main/java/br/com/fiap/gym_time/controller/PostController.java
@@ -3,65 +3,75 @@ package br.com.fiap.gym_time.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+ import org.slf4j.LoggerFactory;
+ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.gym_time.models.Post;
 
 @RestController
+@RequestMapping("/posts")
 public class PostController {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
     // List of all posts
     private List<Post> posts = new ArrayList<>();
 
     // Get all posts
-    @GetMapping("/posts")
+    @GetMapping()
     public List<Post> getPosts() {
         return posts;
     }
-
+    
     // Create a new post
-    @PostMapping("/posts")
+    @PostMapping()
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
-        System.out.println(("Creating post by " + post.getUser()));
+        log.info("Posting...");
         posts.add(post);
         return ResponseEntity.status(201).body(post);
     }
-
+    
     // Get details of a post
-    @GetMapping("/posts/{id}")
-    public ResponseEntity<Post> getPost(@PathVariable Long id) {
-        var post = posts
-        .stream()
-        .filter(p -> p.getId().equals(id))
-        .findFirst();
-
-        if(post.isEmpty()){
-            System.out.println("Post not found");
-            return ResponseEntity.notFound().build(); 
-        }
-
-        return ResponseEntity.ok(post.get());
+    @GetMapping("/{id}")
+    public Post getPostByID(@PathVariable Long id) {
+        log.info("Searching post " + id);
+        return getPost(id);
     }
 
     // Delete a post
-    @PostMapping("/posts/{id}")
-    public ResponseEntity<Post> deletePost(@PathVariable Long id) {
-        var post = posts
-        .stream()
-        .filter(p -> p.getId().equals(id))
-        .findFirst();
-
-        if(post.isEmpty()){
-            System.out.println("Post not found");
-            return ResponseEntity.notFound().build(); 
-        }
-
-        posts.remove(post.get());
-        return ResponseEntity.ok(post.get());
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        log.info("Deleting post " + id);
+        posts.remove(getPost(id));
     }    
+
+    // Update a post
+    @PutMapping("/{id}")
+    public void updatePost(@PathVariable Long id, @RequestBody Post post) {
+        log.info("Updating post " + id);
+        posts.remove(getPost(id));
+        post.setId(id);
+        posts.add(post);
+    }
+
+    private Post getPost(Long id) {
+        return posts
+            .stream()
+            .filter(p -> p.getId().equals(id)) 
+            .findFirst()
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+            );
+
+    }
 }
